5fd366bbe2b2dedaeb31e3f2052c972d1539e28e
sheenamomanyi <sheenamongina@gmail.com>
2024-05-17 11:37:27 +0300

    I deleted extra files

--- Mnazi/mnazi_main.py
+++
@@ -1,4 +0,0 @@
-def mnaziP(name):
-    return name
-
-print("My name is", mnaziP("mnazi"))
--- multiply.py
+++
@@ -1,9 +0,0 @@
-def multiply_numbers(*args):
-    
-    result = 1
-    for num in args:
-        result *= num
-    return result
-
-result = multiply_numbers(2, 3, 4)
-print("Result:", result)  # Output: Result: 24
